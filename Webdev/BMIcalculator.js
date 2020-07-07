cc136d8ebfca18257f1bbb7bb3265fcdfc380552
mhp95 <43661172+mhp95@users.noreply.github.com>
2020-07-07 17:29:28 -0300

    Update BMIcalculator.js

--- Webdev/BMIcalculator.js
+++ Webdev/BMIcalculator.js
@@ -1,5 +1,20 @@
-
-function BMI(weight,height){
+function bmiCalculator (weight, height) {
     var bmi = Math.round(weight/((height)*height));
-    return bmi;
+    var interpretation="";
+    switch(true){
+        case (bmi<18.5):
+            interpretation = "Your BMI is " + bmi + ", so you are underweight";
+            break;
+        case (24.9>bmi>18.5):
+            interpretation = "Your BMI is " + bmi + ", so you are normal weight";
+            break;
+        case (bmi>24.9):
+            interpretation = "Your BMI is " + bmi + ", so you are overweight";
+            break;
+        default:
+            interpretation = "xvzc";
+    }
+    return interpretation;
 }
+
+console.log(bmiCalculator(60,2));
